7dfcf0c75cdfe588e57886d7333baa0a134a2248
Mohebullah Yousofi <mohebullah.yousofi@gmail.com>
2025-04-14 10:13:02 +0430

    move register styles and scripts to assets class

--- inc/classes/class-aquila-theme.php
+++ inc/classes/class-aquila-theme.php
@@ -12,8 +12,8 @@ use AQUILA_THEME\Inc\Traits\Singleton;
 class AQUILA_THEME{
    use Singleton;
    protected function __construct(){
-    //  wp_die('hello');
     //load other classes
+     Assets::get_instance();
      $this->setup_hooks();
    }
    protected function setup_hooks(){
@@ -21,23 +21,5 @@ class AQUILA_THEME{
     /**
      * Actions
      */
-    add_action('wp_enqueue_scripts',[$this,'register_styles']);
-    add_action('wp_enqueue_scripts',[$this,'register_scripts']);
-   }
-   public function register_styles(){
-    // Register Styles
-    wp_register_style('style-css', get_stylesheet_uri(), ['bootstrap-css'], filemtime(AQUILA_DIR_PATH . '/style.css'), 'all');
-    wp_register_style('bootstrap-css', AQUILA_DIR_URI . '/assets/src/library/css/bootstrap.min.css', [], false, 'all');
-    // Enqueue Styles
-    wp_enqueue_style('style-css');
-    wp_enqueue_style('bootstrap-css');   
-  }
-   public function register_scripts(){
-    // Register Scripts
-    wp_register_script('main-js', AQUILA_DIR_URI . '/assets/main.js', ['bootstrap-js'], filemtime(AQUILA_DIR_PATH . '/assets/main.js'), true);
-    wp_register_script('bootstrap-js', AQUILA_DIR_URI . '/assets/src/library/js/bootstrap.bundle.min.js', ['jquery'], false, true);    
-    // Enqueue Scripts
-    wp_enqueue_script('main-js');
-    wp_enqueue_script('bootstrap-js');
    }
 }
--- inc/classes/class-assets.php
+++ inc/classes/class-assets.php
@@ -12,7 +12,8 @@ use AQUILA_THEME\Inc\Traits\Singleton;
 class Assets {
   use Singleton;
   protected function __construct(){
-     Assets::get_instance();
+    //  wp_die('hello');
+    //load other classes
      $this->setup_hooks();
    }
    protected function setup_hooks(){
@@ -20,9 +21,23 @@ class Assets {
     /**
      * Actions
      */
-    // add_action('after_setup_theme',[$this, 'setup_theme']);
+    add_action('wp_enqueue_scripts',[$this,'register_styles']);
+    add_action('wp_enqueue_scripts',[$this,'register_scripts']);
    }
-   public function setup_theme(){
-    //   add_theme_support('title-tag');
+   public function register_styles(){
+    // Register Styles
+    wp_register_style('style-css', get_stylesheet_uri(), ['bootstrap-css'], filemtime(AQUILA_DIR_PATH . '/style.css'), 'all');
+    wp_register_style('bootstrap-css', AQUILA_DIR_URI . '/assets/src/library/css/bootstrap.min.css', [], false, 'all');
+    // Enqueue Styles
+    wp_enqueue_style('style-css');
+    wp_enqueue_style('bootstrap-css');   
+  }
+   public function register_scripts(){
+    // Register Scripts
+    wp_register_script('main-js', AQUILA_DIR_URI . '/assets/main.js', ['bootstrap-js'], filemtime(AQUILA_DIR_PATH . '/assets/main.js'), true);
+    wp_register_script('bootstrap-js', AQUILA_DIR_URI . '/assets/src/library/js/bootstrap.bundle.min.js', ['jquery'], false, true);    
+    // Enqueue Scripts
+    wp_enqueue_script('main-js');
+    wp_enqueue_script('bootstrap-js');
    }
 }
