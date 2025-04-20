7016b37425ec12dafd7c78fb4783a236cbc9edc9
Mohebullah Yousofi <mohebullah.yousofi@gmail.com>
2025-04-20 23:40:53 +0430

    add some important theme support

--- inc/classes/class-aquila-theme.php
+++ inc/classes/class-aquila-theme.php
@@ -26,18 +26,63 @@ class AQUILA_THEME{
    public function setup_theme(){
     //add_theme_support( 'title-tag' ) let wordpress to manage document title which means this theme does not use hard coded title tag and expects wordpress to provide it for us 
     add_theme_support( 'title-tag' );
-
+    
+    //add custom logo feature
     add_theme_support( 'custom-logo', [
       'header-text'          => ['site-title', 'site-description' ],
       'height'               => 100,
-		'width'                => 400,
-		'flex-height'          => true,
-		'flex-width'           => true,
-		'unlink-homepage-logo' => true, 
+      'width'                => 400,
+      'flex-height'          => true,
+      'flex-width'           => true,
+      'unlink-homepage-logo' => true, 
     ] );
+
+    //add custom background feature
     add_theme_support( 'custom-background', [
       'default-color' => 'fff',
       'default-image' => '',
-  ] );
-   }
+   ]);
+   
+   //add post thumbnail feature
+    add_theme_support( 'post-thumbnails');
+   
+    //Instead of refreshing the whole page, only the part of the page with the updated widget is refreshed (partial refresh).
+    add_theme_support( 'customize-selective-refresh-widgets');
+ 
+    //When you enable automatic_feed_links, WordPress adds this to your HTML <head> section
+    add_theme_support( 'automatic_feed_links');
+    
+    //This tells WordPress to output HTML5 markup for certain theme elements. Clean and modern HTML output for better performance, accessibility, and styling flexibility
+    add_theme_support( 'html5',
+     [
+      'comment-list',
+      'comment-form',
+      'search-form',
+      'gallery',
+      'caption',
+      'style',
+      'script'
+    ]
+  );
+
+  //this allows us to link custom style sheet for our editor it looks for editor-styles.css and also we can add our own stylesheet file
+  add_editor_style();
+  
+  //This function enables the default block styles.If you don’t add this, your theme might miss out on important default styles, and blocks could look broken or plain on the frontend
+  add_theme_support('wp_block_styles');
+  
+  /*
+  This allows content creators to use "wide" and "full" alignment options on certain blocks (like images, cover blocks).If you're using a Cover block or Image block, enabling align-wide lets you choose:
+  Wide width – slightly wider than the content area.
+  Full width – stretches the block to the full width of the browser window.
+  */
+  add_theme_support('align-wide');
+  
+ //it sets maximum allowed width for any content of our frontend
+  global $content_width;
+  if(! isset($content_width) ){
+     $content_width= 1240 ;
+  }
+
+ }
 }
