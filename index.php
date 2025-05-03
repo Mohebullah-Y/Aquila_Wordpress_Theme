e37873009f82309e2b604373a10d0d3588acedcb
Mohebullah Yousofi <mohebullah.yousofi@gmail.com>
2025-05-04 02:11:03 +0430

    loop posts in blog Page(index) done

--- index.php
+++ index.php
@@ -7,11 +7,61 @@
 // include_once "header.php";
 get_header();
 ?>
-    <div class="content">
-       Content Index
-    </div>
+   <div id="primary">
+       <main class="site-main mt-5" id="main" role="main">
+          <?php
+            if(have_posts()){
+            ?>
+               <div class="container">
+                  <?php
+                    if(is_home() && ! is_front_page()) {
+                      ?>
+                        <header class="mb-5">
+                            <h1 class="page-title">
+                               <?php single_post_title(); ?>
+                            </h1>
+                        </header>
+                     <?php 
+                    }
+                  ?>
+
+                 <div class="row">
+                     <?php
+                        $index = 0;
+                        $no_of_columns = 3;
+
+                        //Start the loop
+                        while(have_posts()): the_post();
+                           if(0 === $index % $no_of_columns) {
+                              ?>
+                              <div class="col-sm-12 col-md-6 col-lg-4">
+                           <?php
+                           }
+                            ?>
+                              <h3><?php the_title(); ?></h3>
+                              <div>
+                                 <?php the_excerpt(); ?>
+                              </div>
+                            <?php
+                            $index++;
+                              if(0 !== $index && 0 === $index % $no_of_columns){
+                              ?>
+                                </div>
+                              <?php
+                              }
+                        
+                        endwhile;
+                     ?>
+                 </div>
+               </div>
+            <?php
+            }
+          ?>
+       </main>
+   </div>
 <?php
 // include_once "footer.php";
 get_footer();
+
 
 
