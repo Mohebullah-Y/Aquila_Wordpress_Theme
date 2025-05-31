289bd7f38de203c22ebc995ad4f7f6e095172eb5
Mohebullah Yousofi <mohebullah.yousofi@gmail.com>
2025-05-31 11:22:08 +0430

    add post post date and author in blog page

--- template-parts/components/blog/entry-meta.php
+++ template-parts/components/blog/entry-meta.php
@@ -1,1 +1,12 @@
 <?php
+  /**
+   * Template for entry meta
+   * 
+   * @package aquila
+   */
+?>
+
+<div class="entry-meta mb-3">
+    <?php aquila_posted_on(); ?>
+    <?php aquila_posted_by(); ?>
+</div>
